085e4aa30d6ba7286973f357e0743d83bded0a98
Ray Cohen <raycohen@gmail.com>
2016-04-16 16:27:20 -0400

    upgrade ember-cli to 2.4.3

--- config/ember-try.js
+++ config/ember-try.js
@@ -3,46 +3,74 @@ module.exports = {
   scenarios: [
     {
       name: 'default',
-      dependencies: { }
-    },
-    {
-      name: 'ember-1.11.3',
-      dependencies: {
-        'ember': '1.11.3',
-        'ember-load-initializers': 'ember-cli/ember-load-initializers#0.0.2'
+      bower: {
+        dependencies: { }
       }
     },
     {
-      name: 'ember-1.12.1',
-      dependencies: {
-        'ember': '1.12.1'
+      name: 'ember-1.13',
+      bower: {
+        dependencies: {
+          'ember': '~1.13.0'
+        },
+        resolutions: {
+          'ember': '~1.13.0'
+        }
+      }
+    },
+    {
+      name: 'ember-1.12',
+      bower: {
+        dependencies: {
+          'ember': '~1.12.0'
+        },
+        resolutions: {
+          'ember': '~1.12.0'
+        }
+      }
+    },
+    {
+      name: 'ember-1.11',
+      bower: {
+        dependencies: {
+          'ember': '~1.11.0'
+        },
+        resolutions: {
+          'ember': '~1.11.0'
+        }
       }
     },
     {
       name: 'ember-release',
-      dependencies: {
-        'ember': 'components/ember#release'
-      },
-      resolutions: {
-        'ember': 'release'
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#release'
+        },
+        resolutions: {
+          'ember': 'release'
+        }
       }
     },
     {
       name: 'ember-beta',
-      dependencies: {
-        'ember': 'components/ember#beta'
-      },
-      resolutions: {
-        'ember': 'beta'
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#beta'
+        },
+        resolutions: {
+          'ember': 'beta'
+        }
       }
     },
     {
       name: 'ember-canary',
-      dependencies: {
-        'ember': 'components/ember#canary'
-      },
-      resolutions: {
-        'ember': 'canary'
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#canary'
+        },
+        resolutions: {
+          'ember': 'canary'
+        }
       }
     }
   ]

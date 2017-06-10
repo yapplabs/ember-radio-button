2d861fa570c59cbe6ac8aa9073508c8e0e17d4d6
Raymond Cohen <raycohen@gmail.com>
2017-06-10 12:42:02 -0400

    ember-cli 2.12.3

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -8,16 +8,15 @@ const {
 } = Ember;
 
 export default function startApp(attrs) {
-  let application;
 
   let attributes = merge({}, config.APP);
   attributes = merge(attributes, attrs); // use defaults, but you can override;
 
   run(() => {
+    let application;
     application = Application.create(attributes);
     application.setupForTesting();
     application.injectTestHelpers();
+    return application;
   });
-
-  return application;
 }

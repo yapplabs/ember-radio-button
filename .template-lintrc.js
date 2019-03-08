c6752bbb194bc16b74456c713a505d7154b2a951
Brandon Drake <drake@popularpays.com>
2019-03-08 15:54:02 -0700

    Updates template linting rules

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -2,7 +2,13 @@
 
 module.exports = {
   extends: 'recommended',
-  ignore: [
-    'dummy/**'
-  ]
+
+  rules: {
+    'no-bare-strings': false,
+    'no-inline-styles': false,
+    'self-closing-void-elements': false,
+    'no-quoteless-attributes': false,
+    'block-indentation': false,
+    'quotes': false
+  }
 };
